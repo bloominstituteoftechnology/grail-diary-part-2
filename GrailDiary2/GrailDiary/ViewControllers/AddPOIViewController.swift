36ec05b00fd51b35342ebba97c4e3b2838da4c9b
Robert Keffury <bobby.keffury@me.com>
2019-07-30 19:58:58 -0700

    Completed requirements for AddPOIViewController.

--- GrailDiary2/GrailDiary/ViewControllers/AddPOIViewController.swift
+++ GrailDiary2/GrailDiary/ViewControllers/AddPOIViewController.swift
@@ -8,7 +8,21 @@
 
 import UIKit
 
+protocol AddPOIDelegate {
+    func poiWasAdded(_ poi: POI)
+}
+
 class AddPOIViewController: UIViewController {
+    
+    
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var clue1TextField: UITextField!
+    @IBOutlet weak var clue2TextField: UITextField!
+    @IBOutlet weak var clue3TextField: UITextField!
+    
+    var delegate: AddPOIDelegate?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,15 +30,53 @@ class AddPOIViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveTapped(_ sender: UIBarButtonItem) {
+        
+        guard let location = locationTextField.text,
+            let country = countryTextField.text,
+            !location.isEmpty,
+            !country.isEmpty else { return }
+        
+        var poi = POI(location: location, country: country, clues: [])
+        
+        if let clue1 = clue1TextField.text, !clue1.isEmpty {
+            poi.clues.append(clue1)
+        }
+        if let clue2 = clue2TextField.text, !clue2.isEmpty {
+            poi.clues.append(clue2)
+        }
+        if let clue3 = clue3TextField.text, !clue3.isEmpty {
+            poi.clues.append(clue3)
+        }
+        
+        delegate?.poiWasAdded(poi)
+        
     }
-    */
+    
+    @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
 
+}
+
+extension AddPOIViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let text = textField.text, !text.isEmpty {
+            switch textField {
+            case locationTextField:
+                countryTextField.becomeFirstResponder()
+            case countryTextField:
+                clue1TextField.becomeFirstResponder()
+            case clue1TextField:
+                clue2TextField.becomeFirstResponder()
+            case clue2TextField:
+                clue3TextField.becomeFirstResponder()
+            default:
+                textField.resignFirstResponder()
+            }
+        }
+        return false
+    }
+    
 }
