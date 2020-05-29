28c49887f3328b5f24121e9b03d4d4a8d0210805
donnacadi <donna@gohealthyspace.org>
2020-05-28 18:40:14 -0700

    Initial commit of project

---
+++ GrailDiary/GrailDiary/View Controllers/POI.swift
@@ -0,0 +1,9 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by Trencadi on 5/28/20.
+//  Copyright © 2020 ladonnamayfield. All rights reserved.
+//
+
+import Foundation
---
+++ GrailDiary/GrailDiary/View Controllers/POIDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  POIDetailViewController.swift
+//  GrailDiary
+//
+//  Created by Trencadi on 5/28/20.
+//  Copyright © 2020 ladonnamayfield. All rights reserved.
+//
+
+import UIKit
+
+class POIDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
