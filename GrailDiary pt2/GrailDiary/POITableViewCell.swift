d9879c5aca0f1fd8761eb257e525d8bf092ba0df
Craig Belinfante <craigbasketball@aol.com>
2020-05-31 15:33:23 -0700

    need to finish project

---
+++ GrailDiary pt2/GrailDiary/POITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  POITableViewCell.swift
+//  GrailDiary
+//
+//  Created by Craig Belinfante on 5/28/20.
+//  Copyright © 2020 Craig Belinfante. All rights reserved.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ GrailDiary pt2/GrailDiarypt2/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Craig Belinfante on 5/24/20.
+//  Copyright © 2020 Craig Belinfante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
