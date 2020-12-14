507cc37434d73a278f52427ec2534bac4132455b
James McDougall <jameswilliammcdougall@gmail.com>
2020-12-14 07:43:10 -0600

    Step 5 complete

--- Grail Diary/Grail Diary/Views/POITableViewCell.swift
+++ Grail Diary/Grail Diary/Views/POITableViewCell.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class POITableViewCell: UITableViewCell {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var cluesCountLabel: UILabel!
     
+    var poi: POI? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    private func updateViews() {
+        guard let poi = poi else { return }
+        locationLabel.text = poi.location
+        countryLabel.text = poi.country
+        cluesCountLabel.text = "\(poi.clues.count) of clue(s)"
+    }
 
 }
