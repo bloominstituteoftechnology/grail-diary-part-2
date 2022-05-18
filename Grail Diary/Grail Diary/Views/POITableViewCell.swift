35c6c55b2143be28fb12650e277fff9739a2c85f
Waseem Idelbi <idelbiw@gmail.com>
2022-05-17 22:24:50 -0400

    set up all the logic in the POITableViewCell

--- Grail Diary/Grail Diary/Views/POITableViewCell.swift
+++ Grail Diary/Grail Diary/Views/POITableViewCell.swift
@@ -8,16 +8,25 @@
 import UIKit
 
 class POITableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+//MARK: - Properties
+    var poi: POI? {
+        didSet {
+            updateViews()
+        }
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+//MARK: - IBOutlets
+    @IBOutlet weak var locationLabel:   UILabel!
+    @IBOutlet weak var countryLabel:    UILabel!
+    @IBOutlet weak var cluesCountLabel: UILabel!
+    
+//MARK: - Methods
+    func updateViews() {
+        guard let poi = poi else { return }
+        locationLabel.text   = poi.location
+        countryLabel.text    = poi.country
+        cluesCountLabel.text = "\(poi.clues.count) clues"
     }
-
+    
 }
