711fc6d6ad2d7244064f9806ce238804c965e854
Waseem Idelbi <idelbiw@gmail.com>
2022-05-17 21:21:14 -0400

    In the POIsTableViewController, wired up the tableView's outlet, added delegate and data source conformance and implemented protocol methods, and created a poiList array for the tableView

--- Grail Diary/Grail Diary/View Controllers/POIsTableViewController.swift
+++ Grail Diary/Grail Diary/View Controllers/POIsTableViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class POIsTableViewController: UIViewController {
-
+    
+    //MARK: - Properties
+    var poiList: [POI] = []
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    //MARK: - Methods
+    
+    // Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
+} //End of class
 
-}
-
+extension POIsTableViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        poiList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+} //End of extension
