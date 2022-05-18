db46462330a88990e023ea4a5991e896c0092b27
Waseem Idelbi <idelbiw@gmail.com>
2022-05-17 20:51:51 -0400

    copied the part 1 project

---
+++ Grail Diary/Grail Diary/POIsTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Grail Diary
+//
+//  Created by Waseem Idelbi on 5/13/22.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
